17c2486fb2e8ef51e6f9e6df559efd3ac625a554
Scenaristeur <scenrister@galcm>
2021-04-11 09:52:43 +0200

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,20 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="#">Pic</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item to="/">Feed</b-nav-item>
+        <b-nav-item to="/help">Help</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
+        <b-button size="sm" variant="light">Store assets locally</b-button>
+        <b-button size="sm" variant="warning">Enable Notifications</b-button>
+        <!-- <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
@@ -25,13 +27,13 @@
         </b-nav-item-dropdown>
 
         <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
+        
           <template #button-content>
             <em>User</em>
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        </b-nav-item-dropdown> -->
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -39,10 +41,9 @@
 
 <script>
 export default {
-name: 'NavBar'
-}
+  name: "NavBar",
+};
 </script>
 
 <style>
-
 </style>
